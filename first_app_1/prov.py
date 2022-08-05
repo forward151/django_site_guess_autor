f268628a12c0239e60474ace577180f0d9a9b692
forward151 <85546242+forward151@users.noreply.github.com>
2022-08-05 16:44:29 +0300

    Delete prov.py

--- first_app_1/prov.py
+++
@@ -1,9 +0,0 @@
-
-list_los = [1, 2, 3, 4, 5, 6, 7]
-los = 2.5
-x = 1000
-for i in range(len(list_los)):
-    if abs(list_los[i] - los) < x:
-        x = abs(list_los[i] - los)
-        ind_los_i = i
-print(ind_los_i)
